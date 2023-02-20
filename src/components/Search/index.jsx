61d0767dd8713cffd2e53b5a7fe622000b18181e
Evgenii Evseev <ev-ev89@mail.ru>
2023-02-20 13:57:52 +0300

    Сделал верстку инпута с поиском

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RiSearchLine } from 'react-icons/ri';
+
+import styles from './Search.module.scss';
+
+export const Search = () => {
+  return (
+    <label className={styles.label}>
+      <div className={styles.icon}>
+        <RiSearchLine size="20px" opacity={0.5} />
+      </div>
+      <input className={styles.root} placeholder="Поиск пиццы..." />
+    </label>
+  );
+};
