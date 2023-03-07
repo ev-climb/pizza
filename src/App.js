67deba12ae66a5e66f3b333cd3ae3348e7af3d0e
Evgenii Evseev <ev-ev89@mail.ru>
2023-03-07 13:13:22 +0300

    Добавил пагинацию

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import { Home } from './pages/Home';
 import { Cart } from './pages/Cart';
@@ -10,24 +11,29 @@ import axios from 'axios';
 import Skeleton from './components/PizzaBlock/Skeleton';
 import './scss/app.scss';
 
+export const AppContext = React.createContext({});
+
 function App() {
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [searchValue, setSearchValue] = React.useState('');
   const [sortType, setSortType] = React.useState({
     name: 'популярности',
     sort: 'rating',
   });
   const api = 'https://63ef3f0ac59531ccf16b8250.mockapi.io/';
-  console.log(searchValue);
+
   React.useEffect(() => {
     async function fetchData() {
       try {
         setIsLoading(true);
         const [pizzasRes] = await Promise.all([
           axios.get(
-            `${api}pizzas?${categoryId ? `category=${categoryId}` : ''}&sortBy=${sortType.sort}`,
+            `${api}pizzas?page=${currentPage}&limit=4&${
+              categoryId ? `category=${categoryId}` : ''
+            }&sortBy=${sortType.sort}`,
           ),
         ]);
         setIsLoading(false);
@@ -38,7 +44,7 @@ function App() {
       }
     }
     fetchData();
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, currentPage, searchValue]);
 
   const onChangeSearchInput = (e) => {
     setSearchValue(e.target.value);
@@ -53,29 +59,32 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div className="wrapper">
-        <Header setSearchValue={setSearchValue} onChangeSearchInput={onChangeSearchInput} />
-        <div className="content">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Home
-                  sortType={sortType}
-                  setSortType={(id) => setSortType(id)}
-                  renderPizzaBlocks={renderPizzaBlocks}
-                  categoryId={categoryId}
-                  setCategoryId={(id) => setCategoryId(id)}
-                />
-              }
-            />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+    <AppContext.Provider value={{}}>
+      <div className="App">
+        <div className="wrapper">
+          <Header setSearchValue={setSearchValue} onChangeSearchInput={onChangeSearchInput} />
+          <div className="content">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Home
+                    sortType={sortType}
+                    setSortType={(id) => setSortType(id)}
+                    renderPizzaBlocks={renderPizzaBlocks}
+                    categoryId={categoryId}
+                    setCategoryId={(id) => setCategoryId(id)}
+                    setCurrentPage={setCurrentPage}
+                  />
+                }
+              />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </AppContext.Provider>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
+import Pagination from '../components/Pagination';
 
-export const Home = ({ renderPizzaBlocks, categoryId, setCategoryId, sortType, setSortType }) => {
+export const Home = ({
+  renderPizzaBlocks,
+  categoryId,
+  setCategoryId,
+  sortType,
+  setSortType,
+  setCurrentPage,
+}) => {
   return (
     <div className="container">
       <div className="content__top">
@@ -12,6 +20,7 @@ export const Home = ({ renderPizzaBlocks, categoryId, setCategoryId, sortType, s
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{renderPizzaBlocks()}</div>
+      <Pagination onChangePage={(num) => setCurrentPage(num)} />
     </div>
   );
 };
